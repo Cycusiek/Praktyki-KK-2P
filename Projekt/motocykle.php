29140aaba664720830d3ec98d0c2470f219bfc1b
Cycusiek <kacper.kowalczyk6@teb.edu.pl>
2023-11-09 11:47:33 +0100

    Motocykle i ich lista 80% skończone

--- Projekt/motocykle.php
+++ Projekt/motocykle.php
@@ -4,22 +4,123 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="motocykle.css">
     <title>Motocykle</title>
 </head>
 <body>
+        <div id="header">
+           <a href="index.php">Strona Główna</a>
+        </div>
+
     <div id="lista" class="section">
         <h2>Lista dostępnych motocykli</h2>
-        <ul class="lista-motocykli">
-            <li>Kawasaki: Ninja H2</li>
-            <li>Suzuki: GSX-R1000</li>
-            <li>BMW: S1000RR</li>
-            <li>Yamaha: YZF-R1</li>
-            <li>Ducati: Panigale V4 R</li>
-        </ul>
+
+        <div class="motocykl" id="kawasaki">
+            <img src="images/Ninja.jpg" alt="Kawasaki Ninja H2">
+            <p>Kawasaki: Ninja H2</p>
+        </div>
+
+        <div class="motocykl" id="suzuki">
+            <img src="images/suzuki.jpg" alt="Suzuki GSX-R1000">
+            <p>Suzuki: GSX-R1000</p>
+        </div>
+
+        <div class="motocykl" id="bmw">
+            <img src="images/bmw.jpg" alt="BMW S1000RR">
+            <p>BMW: S1000RR</p>
+        </div>
+
+        <div class="motocykl" id="yamaha">
+            <img src="images/yamaha.jpg" alt="Yamaha YZF-R1">
+            <p>Yamaha: YZF-R1</p>
+        </div>
+
+        <div class="motocykl" id="ducati">
+            <img src="images/ducati.jpg" alt="Ducati Panigale V4 R">
+            <p>Ducati: Panigale V4 R</p>
+        </div>
+
+        <div class="motocykl" id="honda">
+            <img src="images/honda cbr600r.jpg" alt="Honda CBR600R">
+            <p>Honda: CBR 600R</p>
+        </div>
+
+        <div class="motocykl" id="aprilia">
+            <img src="images/aprilia RSV4.jpg" alt="Aprilia RSV4">
+            <p>Aprilia: RSV4</p>
+        </div>
+
+        <div class="motocykl" id="kawasaki">
+        <img src="images/Ninja e-1.jpg" alt="Ninja e-1">
+            <p>Kawasaki: Ninja E-1: ABS</p>
+        </div>
+
+        <div class="motocykl" id="suzuki">
+        <img src="images/Suzuki gsx8r.webp" alt="Suzuki gsx8r">
+            <p>Suzuki: GSX8R</p>
+        </div>
+
+        <div class="motocykl" id="bmw">
+            <img src="images/bmw r1250 rs.jpg" alt="Bmw r1250 rs">
+            <p>BMW: R1250 RS</p>
+        </div>
+        <div class="motocykl" id="yamaha">
+        <img src="images/yamaha mt-10.jpg" alt="Yamaha mt-10">
+            <p>Yamaha: MT-10</p>
+        </div>
+        <div class="motocykl" id="ducati">
+            <img src="images/ducati Superleggera V4.jpg" alt="Ducati Superleggera V4">
+            <p>Ducati: Superleggera V4</p>
+        </div>
+
+        <div class="motocykl" id="honda">
+            <img src="images/CBR1000RR-R Fireblade.jpg" alt="Honda CBR1000RR-R">
+            <p>Honda: CBR1000RR-R</p>
+        </div>
+
+        <div class="motocykl" id="aprilia">
+            <img src="images/aprilia RS 125.jpg" alt="Aprilia RS 125 ">
+            <p>Aprilia: RS125</p>
+        </div>
+
+        <div class="motocykl" id="kawasaki">
+        <img src="images/Ninja zx4r.jpg" alt="Ninja ZX4R">
+            <p>Kawasaki: Ninja ZX4R</p>
+        </div>
+
+        <div class="motocykl" id="suzuki">
+            <img src="images/Suzuki gsxr125.webp" alt="Suzuki GSXR 125">
+            <p>Suzuki: GSXR-125</p>
+        </div>
+
+        <div class="motocykl" id="bmw">
+        <img src="images/bmw m1000rr.jpg" alt="Bmw m1000 rr">
+            <p>BMW: M1000 RR</p>
+        </div>
+
+        <div class="motocykl" id="yamaha">
+        <img src="images/yamaha r1m.jpg" alt="Yamaha R1M ">
+            <p>Yamaha: R1M</p>
+        </div>
+
+        <div class="motocykl" id="ducati">
+        <img src="images/ducati panigale v2.jpg" alt="Ducati Panigale V2">
+            <p>Ducati: Panigale V2</p>
+        </div>
+
+        <div class="motocykl" id="honda">
+            <img src="images/cb750 hornet.jpg" alt="Honda CB750 Hornet">
+            <p>Honda CB750 Hornet</p>
+        </div>
+
+        <div class="motocykl" id="aprilia">
+            <img src="images/aprilia RS 660.jpg" alt="Aprilia RS 660">
+            <p>Aprilia: RS 660</p>
+        </div>    
     </div>
 
-    <div>
-        <?php include 'stopka.php';?>
-    </div>
+        <div>
+          <?php include 'stopka.php';?>
+        </div>
 </body>
 </html>
