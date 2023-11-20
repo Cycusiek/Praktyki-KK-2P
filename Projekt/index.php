43af3b9ed22314c4301a02947ab778431b1616e9
Cycusiek <kacper.kowalczyk6@teb.edu.pl>
2023-11-20 13:37:05 +0100

    CSS oraz HTML skończony

--- Projekt/index.php
+++ Projekt/index.php
@@ -11,18 +11,43 @@
 
 <div id="header">
     <div class="header-container">
-            <h1>Wypożyczalnia Motocyklów</h1>
-     <ul>
-        <a href="#">Strona główna</a>
-        <a href="motocykle.php">Motocykle</a>
-        <a href="rezerwacja.php">Rezerwacja</a>
-        <a href="kontakt.php">Kontakt</a>
-     </ul>
- </div>
+        <h1>Wypożyczalnia Motocyklów</h1>
+        <ul>
+            <a href="#">Strona główna</a>
+            <a href="motocykle.php">Motocykle</a>
+            <a href="rezerwacja.php">Rezerwacja</a>
+            <a href="kontakt.php">Kontakt</a>
+        </ul>
+    </div>
 </div>
 
-    <div id="stopka">
-        <p>&copy; 2023 Kacper Kowalczyk</p>
+<div id="main-content" class="additional-info-container">
+    <div class="info-div">
+        <h2>Witaj w naszej wypożyczalni motocykli!</h2>
+        <p>
+            Tutaj znajdziesz szeroki wybór motocykli do wynajęcia. Skorzystaj z naszych usług i przeżyj niezapomniane chwile na dwóch kółkach!
+            sycylia, malaga, a może afryka północna? Wypożycz motocykl na miejscu i przeżyj niesamowitą przygodę! Jedziemy na wyprawę. Swoim czy wypożyczonym motocyklem? Kupujesz nowy motocykl? Najpierw go wynajmij!.
+        </p>
     </div>
+
+    <div class="additional-info">
+        <h3>Nasza Oferta</h3>
+        <p>
+            Oferujemy różnorodne modele motocykli dostosowane do Twoich potrzeb. Bezpieczeństwo i komfort jazdy to nasza priorytetowa sprawa. Sprawdź naszą pełną ofertę motocykli dostępnych do wynajęcia!
+        </p>
+    </div>
+
+    <div class="additional-info">
+        <h3>Przygoda na Kołach</h3>
+        <p>
+            Z nami każda podróż staje się przygodą. Odkryj nowe miejsca, czerp inspirację z krajobrazów i ciesz się wolnością jazdy na dwóch kółkach. Niech motocykl stanie się Twoim wiernym towarzyszem przy każdej wyprawie!
+        </p>
+    </div>
+</div>
+
+<div id="stopka">
+    <p>&copy; 2023 Kacper Kowalczyk</p>
+</div>
+
 </body>
 </html>
